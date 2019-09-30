aeef3f8ccccf9c0829386b3f5a84d6b077ef75dd
Pablo Gramajo <pablo.gramajo@jbknowledge.com>
2019-09-30 13:02:52 -0300

    creating webapi with swagger

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # webapinetcorebase
-net core 3.0
+net core 2.2
+
+comandos importantes:
+```
+dotnet ef migrations add InitialCreate
+
+dotnet ef database update
+```
+
+crear una carpeta Properties
+y crear un archivo launchSettings.json
+deveria de quedar asi:
+Properties/launchSettings.json
+
+copiar en el archivo este contenido reemplazando 
+el valor de Super_Secret_Key 
+```
+{
+  "$schema": "http://json.schemastore.org/launchsettings.json",
+  "iisSettings": {
+    "windowsAuthentication": false, 
+    "anonymousAuthentication": true, 
+    "iisExpress": {
+      "applicationUrl": "http://localhost:34716",
+      "sslPort": 44347
+    }
+  },
+  "profiles": {
+    "IIS Express": {
+      "commandName": "IISExpress",
+      "launchBrowser": true,
+      "launchUrl": "index.html",
+      "environmentVariables": {
+        "ASPNETCORE_ENVIRONMENT": "Development",
+        "Super_Secret_Key": "MY_SUPER_SECRET_KEY"
+      }
+    },
+    "webapinetcorebase": {
+      "commandName": "Project",
+      "launchBrowser": true,
+      "launchUrl": "index.html",
+      "applicationUrl": "https://localhost:5001;http://localhost:5000",
+      "environmentVariables": {
+        "ASPNETCORE_ENVIRONMENT": "Development",
+        "Super_Secret_Key": "MY_SUPER_SECRET_KEY"
+      }
+    }
+  }
+}
+```
+
+para agregar swagger :
+
+```
+    dotnet add package Swashbuckle.AspNetCore.Swagger 
+    dotnet add package Swashbuckle.AspNetCore.SwaggerGen 
+    dotnet add package Swashbuckle.AspNetCore.SwaggerUi 
+```
+
+
---
+++ Startup.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace webapinetcorebase
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().AddJsonOptions(ConfigureJSON);
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "You api title", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    In = "header",
+                    Description = "Please use this formula: 'Bearer + space + JWT' <br> For Example: Bearer xxxxxxxx",
+                    Name = "Authorization",
+                    Type = "apiKey"
+                }
+                );
+                c.AddSecurityRequirement(new Dictionary<string, IEnumerable<string>> {
+                    { "Bearer", Enumerable.Empty<string>() },
+                });
+            });
+        }
+        private void ConfigureJSON(MvcJsonOptions obj)
+        {
+            obj.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+             app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EFExample API V1");
+                c.RoutePrefix = string.Empty;
+            });
+            app.UseMvc();
+        }
+    }
+}
